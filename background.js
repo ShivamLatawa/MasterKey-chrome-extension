5ef15b35ae446490a6b49071a9d85ba16dd41638
Shivam Latawa <slatawa@sapient.com>
2016-01-13 04:37:48 +0530

    added content script code

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-  if (request.method == "getLocalStorage")
-    sendResponse({data: localStorage[request.key]});
-  else
-    sendResponse({});
-});
+window.onLoad = function() {
+    var pswd = chrome.storage.sync.get('value',function(data){
+    	console.log(data);
+    });
+    document.getElementById('savedPswd').innerHTML = pswd;
+}
+
+
+// have access to all the chrome api's and run in completely isolated environment
--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
-  // Trollface image must be at 'my_extension/images/trollface.jpg'.
-  var trollface = chrome.extension.getURL("images/trollface.jpg");
-  $('#content article img').each(function(index, image){
-    $(image).attr('src', trollface);
-  });
+    var $body = $($('body').html()),
+        $submitBtn = $body.find(':submit').eq(1),
+        $form = $("form").eq(2),
+        $formInputUsername = $(":text").eq(2),
+        $formInputPassword = $(":password").eq(0);
+
+    $form.on('submit', function() {
+        var username = $formInputUsername.val(),
+            password = $formInputPassword.val();
+
+        confirm('Do you want to save the password?');
+
+        saveToStorage(password);
+    });
+
+    function saveToStorage(password) {
+        chrome.storage.sync.set({
+            'value': password
+        }, function() {
+            // Notify that we saved.
+            alert('Settings saved');
+        });
+    }
+
 });
